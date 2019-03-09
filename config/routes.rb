c1fa75fdfa7ea474ccd0eb49970ac7373d1269b0
Raghu Betina <raghu@firstdraft.com>
2019-03-09 20:31:05 +0000

    Generate users resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the User resource:
+
+  # CREATE
+  get("/users/new", { :controller => "users", :action => "new_form" })
+  post("/create_user", { :controller => "users", :action => "create_row" })
+
+  # READ
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:id_to_display", { :controller => "users", :action => "show" })
+
+  # UPDATE
+  get("/users/:prefill_with_id/edit", { :controller => "users", :action => "edit_form" })
+  post("/update_user/:id_to_modify", { :controller => "users", :action => "update_row" })
+
+  # DELETE
+  get("/delete_user/:id_to_remove", { :controller => "users", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Improvement plan resource:
 
   # CREATE
